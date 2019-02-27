eea019ce7d8d7884c099f642c843389cc8efdba9
binshion <binshion@hotmail.com>
2019-02-27 17:51:11 +0800

    0227

---
+++ medium/MaxAreaOfIsland_695.java
@@ -0,0 +1,19 @@
+public class MaxAreaOfIsland_695 {
+    public static void main(String[] args){
+        Solution_695 solution = new Solution_695();
+        System.out.println(solution.maxAreaOfIsland(new int[][]{{0,0,1,0,0,0,0,1,0,0,0,0,0},
+                                                                {0,0,0,0,0,0,0,1,1,1,0,0,0},
+                                                                {0,1,1,0,1,0,0,0,0,0,0,0,0},
+                                                                {0,1,0,0,1,1,0,0,1,0,1,0,0},
+                                                                {0,1,0,0,1,1,0,0,1,1,1,0,0},
+                                                                {0,0,0,0,0,0,0,0,0,0,1,0,0},
+                                                                {0,0,0,0,0,0,0,1,1,1,0,0,0},
+                                                                {0,0,0,0,0,0,0,1,1,0,0,0,0}}));
+    }
+}
+
+class Solution_695 {
+    public int maxAreaOfIsland(int[][] grid) {
+        return 0;
+    }
+}
