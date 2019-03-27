0fb523df61736e4edd43e643a85da8a6693a8124
Binshion <binshion@hotmail.com>
2019-03-27 22:40:18 +0800

    CombinationSum_39

--- medium/CombinationSum_39.java
+++ medium/CombinationSum_39.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class CombinationSum_39 {
@@ -17,8 +18,28 @@ public class CombinationSum_39 {
     }
 }
 
+/**
+ * DFS深度优先
+ */
 class Solution_39 {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> lists = new ArrayList<>();
+        Arrays.sort(candidates);
 
+        getLists(lists, new ArrayList<Integer>(), candidates, target, 0);
+
+        return lists;
+    }
+
+    private void getLists(List<List<Integer>> lists, List<Integer> cur, int[] candidates, int target, int startIndex) {
+        if (target > 0) {
+            for (int i = startIndex; i < candidates.length && target >= candidates[i]; i++) {
+                cur.add(candidates[i]);
+                getLists(lists, cur, candidates, target - candidates[i], i);
+                cur.remove(cur.size() - 1);  //回退最后一个  关键点
+            }
+        } else if (target == 0){
+            lists.add(new ArrayList<Integer>(cur));
+        }
     }
 }
